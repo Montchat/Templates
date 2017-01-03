b97e38314a73ea629ef9e8f235be8abfdda375d4
Joe Edwards <jedwards@hanekedesign.com>
2017-01-03 13:25:23 -0500

    create typealias file

--- Templates/ViewControllerExtensions.swift
+++ Templates/ViewControllerExtensions.swift
@@ -6,6 +6,24 @@
 //  Copyright © 2017 HanekeDesign. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-e
+
+//Presenting Alerts
+extension UIViewController {
+	
+	func presentSimpleAlert(withTitle title:Title, andMessage message:Message) {
+		
+		dispatch_async(dispatch_get_main_queue()) {
+			
+			let alertVC = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+			let cancel = UIAlertAction(title: "Ok", style: .Cancel, handler: nil)
+			alertVC.addAction(cancel)
+			
+			self.presentViewController(alertVC, animated: true, completion: nil)
+			
+		}
+		
+	}
+	
+}
