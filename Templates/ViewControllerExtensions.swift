499c0ef57d99607362050ad5ec02ab9fb445e11d
Joe Edwards <jedwards@hanekedesign.com>
2017-01-03 13:22:44 -0500

    init

---
+++ Templates/ViewControllerExtensions.swift
@@ -0,0 +1,11 @@
+//
+//  ViewControllerExtensions.swift
+//  Templates
+//
+//  Created by Joe Edwards on 1/3/17.
+//  Copyright © 2017 HanekeDesign. All rights reserved.
+//
+
+import Foundation
+
+e
